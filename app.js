49a2850237eddcc5e1b5c6fdcae54077eac5e28c
Hartur Barreto Brito <harturbrito92@gmail.com>
2019-06-05 22:15:36 -0300

    Atualização de informações e troca de linguagem

--- app.js
+++ app.js
@@ -45,27 +45,27 @@ app.controller('AboutController', function($scope){
     
     $scope.academicActivities = [
         {
-            name: 'Teaching Assistant of the subject Imperative Programming',
-            from: 'feb 2012',
+            name: 'Monitor de Linguagem de Programação Imperativa',
+            from: 'fev 2012',
             through: 'jun 2012',
-            description: 'Teaching Assistant of the subject Imperative Programming using C as example. The main idea was give some challenges to the students and help them to exercise the concepts.'
+            description: 'Monitor da matéria Linguagem de programação imperativa utilizando C como exemplo. O objetivo da monitoria era propor desafios para a turma e ajudá-los a compreender os conceitos.'
         },
         {
-            name: 'Teaching Assistant of the subject Data Structures',
-            from: 'feb 2015',
+            name: 'Monitor de Estrutura de Dados',
+            from: 'fev 2015',
             through: 'jun 2015',
-            description: 'Teaching Assistant of the subject Data Structures using Java to give the examples. My job was help the students with their doubts and give some challenges to help them to fixate the concepts.'
+            description: 'Monitor da matéria Estrutura de dados utilizando Java para demonstrar. Meu papel era propor desafios e ajudar os alunos com suas dúvidas.'
         },
         {
             name: 'CAPES Scholarship Holder',
-            from: 'aug 2012',
-            through: 'aug 2013',
+            from: 'ago 2012',
+            through: 'ago 2013',
             description: 'Title of the research: Development and application of translucid optical networks performance forecast models.'
         },
         {
             name: 'CAPES Scholarship Holder',
-            from: 'aug 2013',
-            through: 'aug 2014',
+            from: 'ago 2013',
+            through: 'ago 2014',
             description: 'Title of the research: Usage of crosstalk and WSS quantification analytical models.'
         }
     ];
@@ -74,13 +74,13 @@ app.controller('AboutController', function($scope){
         {
             institution: 'Visão',
             finishedOn: '2010',
-            level: 'High School',
+            level: 'Ensino médio',
             link: 'http://www.colegiovisaorecife.com.br/'
         },
         {
-            institution: 'University of Pernambuco (UPE)',
+            institution: 'Universidade de Pernambuco (UPE)',
             finishedOn: '2016',
-            level: 'Bachelor\'s Degree',
+            level: 'Bacharelado',
             link: 'http://www.ecomp.poli.br/'
         }
     ]
@@ -97,52 +97,52 @@ app.controller('DocumentsController', function($scope){
     $scope.documents = [
         {
             name: 'LTE_LTEA.pdf',
-            publish: 'September 2014',
+            publish: 'Setembro 2014',
             link: 'documents/LTE_LTEA.pdf',
             description: 'Document wrote for UPE bachelor\'s degree\'s subject that describe LTE.'
         },
         {
             name: 'DataStructuresIntro.pdf',
-            publish: 'May 2015',
+            publish: 'Maio 2015',
             link: 'documents/DataStructuresIntro.pdf',
             description: 'Document wrote to help students to understand Data Structures when I\'was a teaching assistant volunteer.'
         },
         {
             name: 'Z3TheoremProver.pdf',
-            publish: 'June 2015',
+            publish: 'Junho 2015',
             link: 'documents/Z3TheoremProver.pdf',
             description: 'Document wrote as a project of the subject Formal Methods that describe how Z3 Theorem Prover works.'
         },
         {
             name: 'SysadminUbuntu.pdf',
-            publish: 'July 2015',
+            publish: 'Julho 2015',
             link: 'documents/SysadminUbuntu.pdf',
             description: 'Tutorial of how to create a SVN server using Ubuntu Server. This document was wrote when I was doing the research on 2015 because we need to configure a SVN server to control the version of the code locally. This was usefull for future people that has to maintain the server and to configure other servers.'
         },
         {
             name: 'hbb3-lista1-report.pdf',
-            publish: 'August 2017',
+            publish: 'Agosto 2017',
             link: 'documents/hbb3-lista1-report.pdf',
             description: 'UFPE\'s Master Degree subject Multiple Classifier Systems\' first excercise report.',
             details: 'https://github.com/Hartur/MachineLearning'
         },
         {
             name: 'hbb3-lista2-report.pdf',
-            publish: 'September 2017',
+            publish: 'Setembro 2017',
             link: 'documents/hbb3-lista2-report.pdf',
             description: 'UFPE\'s Master Degree subject Multiple Classifier Systems\' second excercise report.',
             details: 'https://github.com/Hartur/MachineLearning'
         },
         {
             name: 'hbb3-lista3-report.pdf',
-            publish: 'September 2017',
+            publish: 'Setembro 2017',
             link: 'documents/hbb3-lista3-report.pdf',
             description: 'UFPE\'s Master Degree subject Multiple Classifier Systems\' third excercise report.',
             details: 'https://github.com/Hartur/MachineLearning'
         },
         {
             name: 'hbb3-lista4-report.pdf',
-            publish: 'October 2017',
+            publish: 'Outubro 2017',
             link: 'documents/hbb3-lista4-report.pdf',
             description: 'UFPE\'s Master Degree subject Multiple Classifier Systems\' fourth excercise report.',
             details: 'https://github.com/Hartur/MachineLearning'
@@ -155,42 +155,50 @@ jobs = [
     {
         company: 'FITec',
         link: 'http://www.fitec.org.br/pt/inicio/',
-        job: 'Intern',
-        from: 'may 2014',
+        job: 'Estagiário em engenharia de redes',
+        from: 'mai 2014',
         through: 'mar 2015',
-        description: 'There I worked configuring Alcatel Lucent routers.'
+        description: 'Trabalhei como engenheiro de redes focados em roteadores da Lucent Alcatel.'
     },
     {
         company: 'Pitang',
         link: 'https://www.pitang.com/',
-        job: 'Software Developer',
-        from: 'feb 2015',
-        through: 'sep 2016',
-        description: 'There I worked on a Microsoft Excel plugin using C#.'
+        job: 'Estagiário em desenvolvimento de software',
+        from: 'fev 2015',
+        through: 'set 2016',
+        description: 'Trabalhei em uma aplicação que consistia em um plugin para Excel utilizando C#.'
     },
     {
         company: 'ABA - Associação Brasil América',
         link: 'http://www.estudenaaba.com/',
-        job: 'Software Developer',
-        from: 'oct 2016',
+        job: 'Estagiário em desenvolvimento de software',
+        from: 'out 2016',
         through: 'jan 2017',
-        description: 'There I worked on a PHP project that uses Bootstrap 3 and JavaScript as front-end'
+        description: 'Manutenção de sistema que utiliza PHP, JavaScript, Bootstrap e SQL Server.'
     },
     {
         company: 'Sistematech Informática Eireli',
         link: '',
-        job: 'System Analyst',
-        from: 'feb 2017',
+        job: 'Analista de Sistemas',
+        from: 'fev 2017',
         through: 'nov 2017',
-        description: 'The project that I worked there uses ASP.NET Boilerplate, which is a framework that uses Bootstrap 3, AngularJS 1.3 and C#.'
+        description: 'Trabalhei em uma aplicação utilizando Angular 1.3x e Bootstrap no frontend, C# como backend, Entity Framework como ORM, SQL Server como banco, Protractor para testes E2E e testes unitários no backend'
     },
     {
         company: "M2S Software",
         link: "http://www.m2ssoftware.com/",
-        job: "Software Engineer",
+        job: "Engenheiro de Software",
         from: 'nov 2017',
-        through: 'now',
-        description: 'Here I work on a ASP.NET MVC 5 project.'
+        through: 'jan 2019',
+        description: 'Trabalhei em uma aplicação ASP.NET MVC 5 utilizando as linguagens padrões (Razor, JavaScript, Bootstrap) e NHibernate como ORM e SQL Server como banco.'
+    },
+    {
+        company: "Tekap Serviços e Consultoria",
+        link: "https://supertroco.com.br/",
+        job: "Líder Técnico",
+        from: 'nov 2017',
+        through: 'jan 2019',
+        description: 'Desenvolvimento de aplicações utilizando C#, NodeJs, AngularJs e Bootstrap; Gerenciamento de equipe utilizando Scrum adaptado.'
     }
 ]
 
